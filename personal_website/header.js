058cc72484e38d2ae4146bc04847b21089800e66
irisxu10 <xxu10@inside.artcenter.edu>
2025-07-23 23:54:59 +0800

    About Me

--- personal_website/header.js
+++ personal_website/header.js
@@ -2,11 +2,11 @@ document.querySelector('header').innerHTML = `
   <div class="content">
     <div class="logo">IRIS</div>
     <div class="menu-list">
-      <div class="menu-item color-4" onclick="toPage('index.html')">HOME</div>
-      <div class="menu-item color-5" onclick="toPage('about.html')">About Me</div>
+      <div class="menu-item color-2" onclick="toPage('index.html')">HOME</div>
+      <div class="menu-item color-4" onclick="toPage('about.html')">About Me</div>
       <div class="menu-item color-6" onclick="toPage('mdp.html')">MDP Works</div>
       <div class="menu-item color-7" onclick="toPage('writings.html')">Writings</div>
-      <div class="menu-item color-8" onclick="toPage('projects.html')">Projects</div>
+      <div class="menu-item color-5" onclick="toPage('projects.html')">Projects</div>
       <div class="menu-item color-9" onclick="toPage('artworks.html')">Artworks</div>
     </div>
   </div>
