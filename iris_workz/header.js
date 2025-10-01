845c58934148f445164c67b4fe825ff028a5a972
irisxu10 <xxu10@inside.artcenter.edu>
2025-10-01 11:36:04 -0700

    Update header.js

--- iris_workz/header.js
+++ iris_workz/header.js
@@ -34,7 +34,7 @@ document.querySelector('header').innerHTML = `
     <div class="menu-list">
       <div class="menu-item color-2" onclick="toPage('${prefix}index.html')">HOME</div>
       <div class="menu-item color-4" onclick="toPage('${prefix}about.html')">About Me</div>
-      <div class="menu-item color-6" onclick="toPage('${prefix}mdp.html')">MDP Works</div>
+      <div class="menu-item color-6" onclick="toPage('${prefix}mdp.html')">ArtCenter_MDP</div>
       <div class="menu-item color-7" onclick="toPage('${prefix}writings.html')">Writings</div>
       <div class="menu-item color-5" onclick="toPage('${prefix}projects.html')">Projects</div>
       <div class="menu-item color-9" onclick="toPage('${prefix}artworks.html')">Artworks</div>
